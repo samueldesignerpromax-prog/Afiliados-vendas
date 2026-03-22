caaf2b129f8dec5edd623ea3d4a37946c9abbe08
Samuel Tech IA <samueldesignerpromax@gmail.com>
2026-03-22 19:47:47 -0300

    Delete services directory

--- services/init.py
+++
@@ -1,5 +0,0 @@
-from .chatbot import ChatbotIA
-from .pagamento import PagamentoService
-from .afiliado_service import AfiliadoService
-
-__all__ = ['ChatbotIA', 'PagamentoService', 'AfiliadoService']
